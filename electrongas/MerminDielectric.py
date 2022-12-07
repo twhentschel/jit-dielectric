3a633cc7bd9138d10cbbf645959611bc3db19903
Thomas Hentschel <49924808+twhentschel@users.noreply.github.com>
2022-12-07 16:29:26 -0500

    Vectorization of k argument complete

--- electrongas/MerminDielectric.py
+++ electrongas/MerminDielectric.py
@@ -221,8 +221,12 @@ def generalRPAdielectric(k, omega, nu, kBT, mu, dosratio=None):
     if nu.ndim == 0:
         nu = np.expand_dims(nu, axis=0)
         scalar_input = True
-    # Length of array input
-    N = np.max((omega.size, k.size, nu.size))
+    # Lengths of array inputs
+    M = k.size
+    N = omega.size
+    
+    # Meshgrid for broadcasting k, omega
+    k, omega = np.meshgrid(k, omega, indexing='ij', sparse=True)
 
     if dosratio is None:
         # Make dosratio the constant function returning 1
@@ -248,7 +252,7 @@ def generalRPAdielectric(k, omega, nu, kBT, mu, dosratio=None):
 
     # All transformed integrations fall roughly within the same region in the
     # transformed space
-    t = np.linspace(-2.5*np.ones(N), 2.5*np.ones(N), 200)
+    t = np.linspace(-2.5*np.ones((M, N)), 2.5*np.ones((M, N)), 200, axis=0)
     tempwidth = np.sqrt(2*np.abs(mu + 10*kBT))
     realsolve =   np.trapz(realint(t, (np.zeros(N) , p1)), t, axis=0) \
                 + np.trapz(realint(t, (p1, p2)), t, axis=0) \
@@ -263,23 +267,23 @@ def generalRPAdielectric(k, omega, nu, kBT, mu, dosratio=None):
     # "widths" around each point (1e-4 gently smoothes peaks when nu.real == 0)
     nuwidth = nu.real + 1e-4
     # Put these into an array
-    pdiff = np.zeros((8,N))
-    pdiff[1] = np.maximum(p1 - nuwidth, np.zeros(N))
+    pdiff = np.zeros((8, M, N))
+    pdiff[1] = np.maximum(p1 - nuwidth, np.zeros((M, N)))
     pdiff[2] = p1 + nuwidth
-    pdiff[3] = np.maximum(p2 - nuwidth, np.zeros(N))
+    pdiff[3] = np.maximum(p2 - nuwidth, np.zeros((M, N)))
     pdiff[4] = p2 + nuwidth
-    pdiff[5] = np.maximum(p3 - tempwidth, np.zeros(N))
+    pdiff[5] = np.maximum(p3 - tempwidth, np.zeros((M, N)))
     pdiff[6] = p3 + tempwidth
     pdiff[7] = p2 + nuwidth + tempwidth
     # Sort the difficult points, so they are in order
     pdiff = np.sort(pdiff, axis=0)
     # Linearly interpolate between the difficult point +/- their widths to
     # create a set of integration regions bounded by these points (+/- widths)
-    intregions = np.linspace(pdiff[0:7], pdiff[1:8], 100)
+    intregions = np.linspace(pdiff[0:7], pdiff[1:8], 100, axis=0)
     # Integrate within each of the regions
     imagintegrateregions = np.trapz(imagint(intregions), intregions, axis=0)
     # Add up the integrations between each region, resulting in an array of
-    # length N
+    # shape (M,N)
     imagsolve = np.sum(imagintegrateregions, axis=0)
 
     ret = 1j * 2 / np.pi / k**3 * imagsolve
@@ -318,8 +322,10 @@ def generalMermin(epsilon, k, omega, nu, *args):
     _______
         : float
     """
+    N = len(omega)
+    
     epsnonzerofreq = epsilon(k, omega, nu, *args)
-    epszerofreq    = epsilon(k, 0, 0, *args)
+    epszerofreq    = epsilon(k, np.zeros(N), np.zeros(N), *args)
 
     # If nu is zero, we expect to get back epsnonzerofreq. But if omega also
     # equals zero, this code fails. Add a little delta to omega to avoid this.
